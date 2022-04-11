250470572561f2859fe087add2cf94f9adcd28ec
Sergey <antonov2703@mail.ru>
2022-04-12 02:04:38 +0300

    les10

--- les1111/src/test/java/llll/SignUpPageTest.java
+++ les1111/src/test/java/llll/SignUpPageTest.java
@@ -7,7 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.time.Duration;
 
 public class SignUpPageTest {
@@ -17,28 +16,23 @@ public class SignUpPageTest {
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
         driver.manage().window().maximize();
-
         driver.get("https://diary.ru/user/registration");
-
         signUpPage = new SignUpPage(driver);
     }
-
+    @Test
+    public void signUpWithCorrectEmail() {
+        signUpPage.registerWithoutLogin("baron2703@mail.ru");
+        String textError = signUpPage.getTextError();
+        Assert.assertEquals("", textError);
+    }
     @Test
     public void signUpWithIncorrectEmail() {
         signUpPage.registerWithoutLogin("6667666");
         String textError = signUpPage.getTextError();
         Assert.assertEquals("Значение «E-mail» не является правильным email адресом.", textError);
-    }
-
-    @Test
-    public void signUpWithCorrectEmail() {
-        signUpPage.registerWithoutLogin("bububndic3@mail.ru");
-        String textError = signUpPage.getTextError();
-        Assert.assertEquals("", textError);
     }
 
     @After
